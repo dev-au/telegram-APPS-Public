d88486b7e6b9f2895c3d7d63a3422ae1791a38c2
dscodeuz <107607543+dscode-uz@users.noreply.github.com>
2023-06-23 10:41:33 +0500

    Update start_user.py

--- handlers/users/start_user.py
+++ handlers/users/start_user.py
@@ -1,1 +1,13 @@
-from imports import *from sqlite3 import IntegrityError@dp.message_handler(CommandStart())async def start_bot_command(message: types.Message):    user_id = message.from_user.id    text = message.text    try:        db.add_user(user_id, message.from_user.language_code)        await message.answer(f"Xush kelibsiz {message.from_user.full_name}")    except IntegrityError:        await message.answer("Salom")
+from imports import *
+from sqlite3 import IntegrityError
+
+
+@dp.message_handler(CommandStart())
+async def start_bot_command(message: types.Message):
+    user_id = message.from_user.id
+    text = message.text
+    try:
+        db.add_user(user_id, message.from_user.language_code)
+        await message.answer(f"Xush kelibsiz {message.from_user.full_name}")
+    except IntegrityError:
+        await message.answer("Salom")
