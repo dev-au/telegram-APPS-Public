74530863a4088f6faf1cbf087b6c6ce1a28c4017
dscodeuz <107607543+dscode-uz@users.noreply.github.com>
2023-06-23 10:41:06 +0500

    Update errors.py

--- handlers/errors.py
+++ handlers/errors.py
@@ -1,1 +1,17 @@
-from loguru import loggerfrom imports import *@dp.errors_handler()async def errors_handler(update, exception):    """    Exceptions handler. Catches all    exceptions within task factory tasks.    """    logger.add("logs.txt")    logger.exception(exception)    logger.debug(update)    await dp.bot.send_message(chat_id=ADMINS[0], text=f"ERROR!\n{exception}\n\n<code>{update}</code>")    file = types.InputFile(path_or_bytesio="logs.txt")    await asyncio.sleep(0.05)    await dp.bot.send_document(chat_id=ADMINS[0],document=file)    return True
+from loguru import logger
+from imports import *
+
+@dp.errors_handler()
+async def errors_handler(update, exception):
+    """
+    Exceptions handler. Catches all
+    exceptions within task factory tasks.
+    """
+    logger.add("logs.txt")
+    logger.exception(exception)
+    logger.debug(update)
+    await dp.bot.send_message(chat_id=ADMINS[0], text=f"ERROR!\n{exception}\n\n<code>{update}</code>")
+    file = types.InputFile(path_or_bytesio="logs.txt")
+    await asyncio.sleep(0.05)
+    await dp.bot.send_document(chat_id=ADMINS[0],document=file)
+    return True
