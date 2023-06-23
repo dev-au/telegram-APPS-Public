45e665241fcfc01e1c1bc27236eb30223b6ab8ee
dscodeuz <107607543+dscode-uz@users.noreply.github.com>
2023-06-23 10:42:33 +0500

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,1 +1,12 @@
-import asynciofrom aiogram import executorfrom aiogram import typesfrom aiogram.dispatcher import FSMContextfrom aiogram.dispatcher.filters import Command, CommandStart, CommandHelpfrom utils_bot import *from data import *from keyboards import inline_keyboards, default_keyboardsimport filters
+import asyncio
+
+from aiogram import executor
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Command, CommandStart, CommandHelp
+
+
+from utils_bot import *
+from data import *
+from keyboards import inline_keyboards, default_keyboards
+import filters
