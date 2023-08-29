f6497e3c56d46490a2ad8ac24c5197371901f47f
dscodeuz <107607543+dscode-uz@users.noreply.github.com>
2023-08-29 09:46:00 +0500

    Delete utils_bot directory

--- utils_bot/__init__.py
+++
@@ -1,4 +0,0 @@
-from .loader import bot, dp, db
-from .notify_admins import admins_note
-from .bot_commands import set_bot_commands
-from .throttling import rate_limit
--- utils_bot/bot_commands.py
+++
@@ -1,7 +0,0 @@
-async def set_bot_commands(bot_commands: dict):
-    from aiogram.types import BotCommand
-    from .loader import dp
-    commands_list = []
-    for command_name, command_info in bot_commands.items():
-        commands_list.append(BotCommand(command_name, command_info))
-    await dp.bot.set_my_commands(commands_list)
