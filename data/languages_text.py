4023906d11c374cc3409b610720020de05679269
Abdulloh Umar <107607543+dev-au@users.noreply.github.com>
2024-02-23 21:32:49 +0500

    Update languages_text.py

--- data/languages_text.py
+++ data/languages_text.py
@@ -1,1 +1,14 @@
-languages = ["uz", "en"]all_text = {    "hello":        [            "Salom {name}",            "Hello {name}"        ],    "wrt":        [            "siz yozdingiz {text}",            "you write {text}",        ]}
+languages = ["uz", "en"]
+all_text = {
+    "hello":
+        [
+            "Salom {name}",
+            "Hello {name}"
+        ],
+
+    "wrt":
+        [
+            "siz yozdingiz {text}",
+            "you write {text}",
+        ]
+}
