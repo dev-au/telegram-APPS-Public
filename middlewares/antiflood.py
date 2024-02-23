28b8c3c628d9b8f0be847e6647e60286e404826d
Abdulloh Umar <107607543+dev-au@users.noreply.github.com>
2024-02-23 21:31:50 +0500

    Update antiflood.py

--- middlewares/antiflood.py
+++ middlewares/antiflood.py
@@ -1,1 +1,27 @@
-from typing import Any, Awaitable, Callable, Dictfrom aiogram import BaseMiddlewarefrom aiogram.dispatcher.flags import get_flagfrom aiogram.types import Messagefrom cachetools import TTLCacheclass ThrottlingMiddleware(BaseMiddleware):    caches = {        "spin": TTLCache(maxsize=10_000, ttl=2),        "default": TTLCache(maxsize=10_000, ttl=1)    }    async def __call__(            self,            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],            event: Message,            data: Dict[str, Any],    ) -> Any:        throttling_key = get_flag(data, "throttling_key")        if throttling_key is not None and throttling_key in self.caches:            if event.chat.id in self.caches[throttling_key]:                return            else:                self.caches[throttling_key][event.chat.id] = None        return await handler(event, data)
+from typing import Any, Awaitable, Callable, Dict
+
+from aiogram import BaseMiddleware
+from aiogram.dispatcher.flags import get_flag
+from aiogram.types import Message
+from cachetools import TTLCache
+
+
+class ThrottlingMiddleware(BaseMiddleware):
+    caches = {
+        "spin": TTLCache(maxsize=10_000, ttl=2),
+        "default": TTLCache(maxsize=10_000, ttl=1)
+    }
+
+    async def __call__(
+            self,
+            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+            event: Message,
+            data: Dict[str, Any],
+    ) -> Any:
+        throttling_key = get_flag(data, "throttling_key")
+        if throttling_key is not None and throttling_key in self.caches:
+            if event.chat.id in self.caches[throttling_key]:
+                return
+            else:
+                self.caches[throttling_key][event.chat.id] = None
+        return await handler(event, data)
