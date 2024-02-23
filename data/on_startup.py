872fcce7bb4fda80beb51dcbf9bc47f3309926bd
Abdulloh Umar <107607543+dev-au@users.noreply.github.com>
2024-02-23 21:33:01 +0500

    Update on_startup.py

--- data/on_startup.py
+++ data/on_startup.py
@@ -1,1 +1,12 @@
-def runner_admin_message():    return f"This bot successfully restarted!"def runner_console_message(bot_username: str):    return f"https://t.me/{bot_username} is running!"def bot_commands():    return {        "start": "Botni yangilash"    }
+def runner_admin_message():
+    return f"This bot successfully restarted!"
+
+
+def runner_console_message(bot_username: str):
+    return f"https://t.me/{bot_username} is running!"
+
+
+def bot_commands():
+    return {
+        "start": "Botni yangilash"
+    }
