204dbba8c8ca3a0f18c01f431cd55ea229db82bf
Abdulloh Umar <107607543+dev-au@users.noreply.github.com>
2024-02-23 21:51:50 +0500

    Update current_bot.py

--- current_bot.py
+++ current_bot.py
@@ -1,1 +1,16 @@
-from aiogram.enums import ParseModefrom environs import Envfrom aiogram import (types, filters, Router, Dispatcher, Bot)from bot_spec import local_translatorfrom data.languages_text import languages, all_textfrom data.sqlite_db import UsersTableenv = Env()env.read_env()BOT_TOKEN = env.str("BOT_TOKEN")ADMINS = list(map(int, env.list("ADMINS")))bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)dp = Dispatcher()_ = local_translator.LanguageTranslator(languages, all_text)user_table = UsersTable()
+from aiogram.enums import ParseMode
+from environs import Env
+from aiogram import (types, filters, Router, Dispatcher, Bot)
+from bot_spec import local_translator
+from data.languages_text import languages, all_text
+from data.sqlite_db import UsersTable
+
+env = Env()
+env.read_env()
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMINS = list(map(int, env.list("ADMINS")))
+bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
+_ = local_translator.LanguageTranslator(languages, all_text)
+
+user_table = UsersTable()
