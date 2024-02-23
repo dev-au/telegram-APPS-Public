15bd82dedbdc1dd14370df85ac74b388d9349590
Abdulloh Umar <107607543+dev-au@users.noreply.github.com>
2024-02-23 21:34:41 +0500

    Update __init__.py

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,1 +1,5 @@
-from .antiflood import ThrottlingMiddlewarefrom current_bot import dpif __name__ == "middlewares":    dp.message.middleware(ThrottlingMiddleware())
+from .antiflood import ThrottlingMiddleware
+from current_bot import dp
+
+if __name__ == "middlewares":
+    dp.message.middleware(ThrottlingMiddleware())
