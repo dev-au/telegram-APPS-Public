a679567fdc8e7bea752841ed47f9a34d6dcc3023
Abdulloh Umar <107607543+dev-au@users.noreply.github.com>
2024-02-23 21:33:49 +0500

    Update help_command.py

--- handlers/users/help_command.py
+++ handlers/users/help_command.py
@@ -1,1 +1,6 @@
-from current_bot import _, types, filters, dp@dp.message(filters.Command("help"))async def help_command(message: types.Message):    await message.answer("you get help")
+from current_bot import _, types, filters, dp
+
+
+@dp.message(filters.Command("help"))
+async def help_command(message: types.Message):
+    await message.answer("you get help")
